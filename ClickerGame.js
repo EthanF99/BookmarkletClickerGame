f767e94cef55b2b62996c7ef6f9060ea630adeab
EthanF99 <EthanF99@users.noreply.github.com>
2023-03-27 17:52:04 -0400

    Box with Incrementing Num

--- ClickerGame.js
+++ ClickerGame.js
@@ -1,5 +1,22 @@
 (function(){
     console.log("Hi, it's working right now");
-    alert("Hello");
-    alert("how are you");
+    let box = document.createElement('div');
+    box.style.position = 'fixed';
+    box.style.bottom = '50px';
+    box.style.right = '10px';
+    box.style.width = '150px';
+    box.style.height = '100px';
+    box.style.color = 'black';
+    box.style.background = 'lightblue';
+    box.style.padding = '20px';
+    document.body.appendChild(box);
+
+    let money = 0;
+    loop();
+
+    function loop(){
+        box.innerText = money.toString();
+        money++;
+        setTimeout(loop, 33);
+    }
 })();
